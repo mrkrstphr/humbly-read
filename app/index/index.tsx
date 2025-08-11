f9d81651086ee4a779c0335247a15f59f9cc658e
Kristopher Wilson <kristopherwilson@gmail.com>
2025-08-10 20:26:09 -0400

    stuff

--- app/index/index.tsx
+++ app/index/index.tsx
@@ -32,6 +32,17 @@ export function IndexPage() {
     <main className="p-4">
       <h1 className="text-2xl font-bold mb-4">Humbly Read</h1>
 
+      <p className="mb-4">
+        I buy a lot of comics on{" "}
+        <a href="https://www.humblebundle.com/books" target="_blank">
+          Humble Bundle
+        </a>{" "}
+        and <a href="https://www.fanatical.com/en/bundle/books">Fanatical</a>,
+        but I don't read a lot of them. So I made this to keep track of things
+        I've purchased and read to try to convince myself to stop throwing away
+        money.
+      </p>
+
       <div className="bg-gray-500 dark:bg-gray-700 h-5 w-full rounded">
         <div
           className={`${colorByPercent(percentCompleted)} h-5 rounded`}
--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="dark:bg-gray-900 text-gray-100">
+      <body className="dark:bg-gray-900 dark:text-gray-100">
         {children}
         <ScrollRestoration />
         <Scripts />
