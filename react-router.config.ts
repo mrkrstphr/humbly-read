3f237dcca9fff95282bdcec6ef26c656f57f0e01
Kristopher Wilson <kristopherwilson@gmail.com>
2025-08-10 21:37:05 -0400

    sans a forward slash

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  basename: "/humbly-read/",
+  basename: "/humbly-read",
   async prerender() {
     return ["/"];
   },
